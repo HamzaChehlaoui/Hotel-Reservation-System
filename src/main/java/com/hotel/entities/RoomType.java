a8af3d4b3228a70202fe40990a4926c3cfc8042e
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-14 14:08:13 +0100

    feat: Override toString() in RoomType for better readability #2

--- src/main/java/com/hotel/entities/RoomType.java
+++ src/main/java/com/hotel/entities/RoomType.java
@@ -3,5 +3,10 @@ package com.hotel.entities;
 public enum RoomType {
     STANDARD_SUITE,
     JUNIOR_SUITE,
-    MASTER_SUITE
+    MASTER_SUITE;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase().replace("_", " ");
+    }
 }
