1f8ab39bebbe80f6320c1f654d5a61e5fd9ce13a
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-14 14:25:46 +0100

    feat: Complete immutable Room entity implementation Closes #2

--- src/main/java/com/hotel/entities/Room.java
+++ src/main/java/com/hotel/entities/Room.java
@@ -1,4 +1,56 @@
 package com.hotel.entities;
 
+import java.util.Date;
+
 public class Room {
+
+    private final int roomNumber;
+    private final RoomType roomType;
+    private final int pricePerNight;
+    private final Date creationDate;
+
+    public Room(int roomNumber , RoomType roomType , int pricePerNight ) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.pricePerNight = pricePerNight;
+        this.creationDate = new Date();
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public Date getCreationDate(){
+        return new Date(creationDate.getTime());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Room room = (Room) obj;
+        return roomNumber == room.roomNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(roomNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "number=" + roomNumber +
+                ", type=" + roomType +
+                ", price=" + pricePerNight +
+                '}';
+    }
 }
