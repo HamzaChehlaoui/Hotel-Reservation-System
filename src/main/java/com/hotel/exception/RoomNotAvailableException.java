6a5d79d040c444a04d9b5fe534f830c536305092
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-15 12:14:18 +0100

    feat: add exception RoomNotAvailableException

--- src/main/java/com/hotel/exception/RoomNotAvailableException.java
+++ src/main/java/com/hotel/exception/RoomNotAvailableException.java
@@ -1,4 +1,7 @@
 package com.hotel.exception;
 
-public class RoomNotAvailableException {
+public class RoomNotAvailableException extends HotelReservationException {
+    public RoomNotAvailableException(int roomNumber) {
+        super("Room " + roomNumber + " is not available for the specified period");
+    }
 }
